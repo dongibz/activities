2a8e89d0b4a6b5beb7cf65ddcb9ef21218dfa1eb
dongibz <dglimlianting@gmail.com>
2015-05-06 16:37:17 +0800

    Login with Admin & User Servlet Activity

---
+++ HelloWorld_1/src/java/com/servlet/HelloWorldServlet.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author asi
+ */
+public class HelloWorldServlet extends HttpServlet{
+    public void init() throws ServletException{
+        
+    }
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        
+        String type = request.getParameter("type");
+        
+        switch (type) {
+            case "admin":
+                {
+                    RequestDispatcher rd = request.getRequestDispatcher("/ProcessAdminServlet");
+                    rd.forward(request, response);
+                    break;
+                }
+            case "user":
+                {
+                    RequestDispatcher rd = request.getRequestDispatcher("/ProcessUserServlet");
+                    rd.forward(request, response);
+                    break;
+                }
+        }
+            
+    }
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doGet(request, response);
+    }
+    
+    public void destroy(){
+        
+    }    
+}
---
+++ HelloWorld_2/src/java/com/servlet/HelloWorldServlet.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author asi
+ */
+public class HelloWorldServlet extends HttpServlet{
+    public void init() throws ServletException{
+        
+    }
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        PrintWriter out = response.getWriter();
+        String name = request.getParameter("name");
+        String course = request.getParameter("course");
+        String languages[] = request.getParameterValues("language");
+        String champion = request.getParameter("champ");
+        out.println("<h1>Hello "+name+"!</h1>");
+        out.println("<h2>Are you sure you will graduate "+course+"?");
+        out.println("<h3>You should learn</h3>");      
+        for(String langs : languages){
+            out.print(langs+"<br>");
+        }
+        if(champion.equals("Pacman")){            
+            out.println("<h3>Your champ is "+champion+"!</h3>");
+        }else{
+            out.println("<h3>"+champion+" cheated the Fight!</h3>");
+        }
+            
+    }
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        doGet(request, response);
+    }
+    
+    public void destroy(){
+        
+    }    
+}
